aa90fbcb48964d35a07ce08a2502681a2f0fae2c
tsubasa283paris <tsubasa283paris@gmail.com>
2023-07-23 03:37:43 +0900

    Update circleci config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Successfully Deployed <code>A React application</code> to Netlify with CircleCI
         </p>
         <a
           className="App-link"
